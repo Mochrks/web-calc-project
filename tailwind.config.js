8a7bbf37c472f2cd4e749127d1d0c5b2385741c7
mochrks <mochrizkiks@gmail.com>
2024-09-01 11:56:31 +0700

    feat: add dummy layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
