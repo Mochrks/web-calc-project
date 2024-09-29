6e75a34aad82fd236c62266fcbac5433f405681e
mochrks <mochrizkiks@gmail.com>
2024-09-29 22:18:24 +0700

    update: style projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,17 @@
-import React from "react";
 
-import { Typography, Flex, Button, Carousel, Card, Col, Row }
-  from
-  "antd"
-  ;
+import './App.css'
+import { Button } from '@/components/ui/button'
 
-
-const { Title } = Typography;
-
-
-const contentStyle: React.CSSProperties = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-}
-
-const textStyle: React.CSSProperties = {
-  color: '#fff',
-}
 function App() {
+
+
   return (
-
     <>
-      {/* navbar */}
-
-      <nav className="container mx-auto flex flex-row  bg-blue-200 ">
-        <div className="flex-grow p-3 bg-red-900">
-          <Title level={4} style={textStyle}>Calc</Title>
-        </div>
-        <div className="flex p-3 bg-blue-900">
-          <ul className="flex flex-row gap-10">
-            <li>Home</li>
-            <li>Calculator</li>
-            <li>BMI Calculator</li>
-            <li>Income Calculator</li>
-            <li>About</li>
-          </ul>
-        </div>
-      </nav >
-
-
-      <main className="container mx-auto bg-blue-200">
-        <section id="hero">
-          <Flex gap="middle">
-            <Flex gap="middle" className="py-20 pl-5 bg-red-500" vertical >
-              <div>
-                <Title style={textStyle}>Simplify Your Calculations</Title>
-                <Title level={5} style={textStyle}>Experience the ultimate convenience with our all-in-one calculator app. From basic arithmetic to BMI and income calculations, Calc has you covered. Start optimizing your time and decisions today!</Title>
-                <Button type="primary" >Primary Button</Button>
-              </div>
-            </Flex>
-            <Flex className="bg-yellow-200">
-              <Title level={5} style={textStyle}>Experience the ultimate convenience with our all-in-one calculator app. From basic arithmetic to BMI and income calculations, Calc has you covered. Start optimizing your time and decisions today!</Title>
-
-              <img src="" alt="" />
-            </Flex>
-          </Flex>
-        </section>
-
-
-        <section id="corousel">
-          <div>
-            <Carousel autoplay>
-              <div>
-                <h3 style={contentStyle}>1</h3>
-              </div>
-              <div>
-                <h3 style={contentStyle}>2</h3>
-              </div>
-              <div>
-                <h3 style={contentStyle}>3</h3>
-              </div>
-              <div>
-                <h3 style={contentStyle}>4</h3>
-              </div>
-            </Carousel>
-          </div>
-
-        </section>
-
-
-        <section id="about">
-          <Flex className="flex flex-col gap-5 p-10">
-            <Title style={textStyle}>Guidelines For Calc APP</Title>
-            <Title level={5} style={textStyle}>Calc is your go-to tool for all your calculation needs. Whether you're crunching numbers, calculating your Body Mass Index (BMI), or figuring out your income, Calc offers an intuitive and efficient experience. Designed with simplicity in mind, our app ensures accurate results, helping you make informed decisions with ease. Discover how Calc can simplify your day-to-day tasks</Title>
-          </Flex>
-        </section>
-
-
-        <section id="features" className="mt-10 px-10">
-          <Flex vertical>
-            <Row gutter={16}>
-              <Col span={8}>
-                <Card bordered={false} style={textStyle} className="bg-[#0694F4]">
-                  Card content
-                </Card>
-              </Col>
-              <Col span={8}>
-                <Card bordered={false} style={textStyle} className="bg-[#06D99C]">
-                  Card content
-                </Card>
-              </Col>
-              <Col span={8}>
-                <Card bordered={false} style={textStyle} className="bg-[#F5B901]">
-                  Card content
-                </Card>
-              </Col>
-            </Row>
-
-
-          </Flex>
-
-        </section>
-
-
-
-      </main>
-
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,57 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{ts,tsx,jsx}",
+    "./components/**/*.{ts,tsx,jsx}",
+    "./app/**/*.{ts,tsx,jsx}",
+    "./src/**/*.{ts,tsx,jsx}",
   ],
   theme: {
+    container: {
+      center: "true",
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      fontFamily: {
-        poppins: [
-          "Poppins",
-          "system-ui",
-          "Avenir",
-          "Helvetica",
-          "Arial",
-          "sans-serif",
-        ],
+      colors: {
+        main: "#a388ee",
+        mainAccent: "#9e66ff", // not needed for shadcn components
+        overlay: "rgba(0,0,0,0.8)", // background color overlay for alert dialogs, modals, etc.
+
+        // light mode
+        bg: "#e3dff2",
+        text: "#000",
+        border: "#000",
+
+        // dark mode
+        darkBg: "#1D1F27",
+        darkText: "#eeefe9",
+        darkBorder: "#000",
+        secondaryBlack: "#1b1b1b", // opposite of plain white, not used pitch black because borders and box-shadows are that color
+      },
+      borderRadius: {
+        base: "6px",
+      },
+      boxShadow: {
+        light: "2px 6px 0px 0px #000",
+        dark: "2px 6px 0px 0px #000",
+      },
+      translate: {
+        boxShadowX: "2px",
+        boxShadowY: "6px",
+        reverseBoxShadowX: "-2px",
+        reverseBoxShadowY: "-6px",
+      },
+      fontWeight: {
+        base: "600",
+        heading: "700",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
